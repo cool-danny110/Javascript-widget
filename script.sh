f2ebceee428a2fba73ebdbf5f4e341d3d8c97dd3
minotaurs110 <predy186877@gmail.com>
2022-12-14 06:52:34 -0800

    final commit

---
+++ script.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+NEW_NAME="minotaurs110"
+NEW_EMAIL="predy186877@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" != "$NEW_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" != "$NEW_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
